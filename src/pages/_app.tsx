f13cd5f6a3ef02201ea1964e89da4cbc97c0f97d
stalugaexpert <stalegapawel@gmail.com>
2022-10-03 19:40:16 +0200

    AS-29: Added test changing theme function using scss.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,16 @@
 import '@styles/globals.scss'
 
+// import "../styles/index.scss"
 import type { AppProps } from 'next/app'
 import { appWithTranslation } from 'next-i18next'
 
+import ThemeProvider from "../contexts/ThemeProvider"
+
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />
-}
+  return (
+    <ThemeProvider>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )}
 
 export default appWithTranslation(MyApp)
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,18 @@
 import styles from '@styles/index.module.scss'
+// import styles from '@styles/abstracts/test.module.scss'
 import type { NextPage } from 'next'
 import { useTranslation } from 'next-i18next'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 
 const Home: NextPage = () => {
   const { t } = useTranslation()
+  console.log(styles)
 
   return (
-    <div className={styles.HomePage}>
+    // <div className={styles.HomePage}>
+    //   <p>{t('common:testNamespace')}</p>
+    // </div>
+    <div className={`${styles.myClass}`}>
       <p>{t('common:testNamespace')}</p>
     </div>
   )
