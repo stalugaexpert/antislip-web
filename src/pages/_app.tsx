d656e199342c8b2d37876b97c60869c7d339cd31
stalugaexpert <stalegapawel@gmail.com>
2022-10-09 19:24:13 +0200

    AS-29: Added tailwind css, added switch theme functionality, added theme properties.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,12 @@
-import '@styles/globals.scss'
+import '@styles/globals.css'
 
-// import "../styles/index.scss"
 import type { AppProps } from 'next/app'
 import { appWithTranslation } from 'next-i18next'
-
-import ThemeProvider from "../contexts/ThemeProvider"
+import { ThemeProvider } from 'next-themes'
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   return (
-    <ThemeProvider>
+    <ThemeProvider attribute="class">
       <Component {...pageProps} />
     </ThemeProvider>
   )}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,22 @@
-import styles from '@styles/index.module.scss'
-// import styles from '@styles/abstracts/test.module.scss'
 import type { NextPage } from 'next'
 import { useTranslation } from 'next-i18next'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
+import { useTheme } from 'next-themes'
 
 const Home: NextPage = () => {
   const { t } = useTranslation()
-  console.log(styles)
+  const { systemTheme, theme, setTheme } = useTheme()
+  const currentTheme = theme === 'system' ? systemTheme : theme
 
   return (
-    // <div className={styles.HomePage}>
-    //   <p>{t('common:testNamespace')}</p>
-    // </div>
-    <div className={`${styles.myClass}`}>
-      <p>{t('common:testNamespace')}</p>
+    <div className='flex flex-col items-center justify-center h-screen bg-neutral50 dark:bg-neutral900'>
+      <p className='text-4xl mb-5 text-neutral900 dark:text-neutral50'>{t('common:testNamespace')}</p>
+      <button
+        className='py-3 px-4 rounded-3xl bg-neutral900 dark:bg-neutral50 text-neutral50 dark:text-neutral900'
+        onClick={() => setTheme(currentTheme === 'light' ? 'dark' : 'light')}
+      >
+        {t('common:changeTheme')}
+      </button>
     </div>
   )
 }
