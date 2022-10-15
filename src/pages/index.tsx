443768e52ca95508eb56fe86c657b446cece806e
stalugaexpert <stalegapawel@gmail.com>
2022-10-15 15:58:14 +0200

    AS-14: Removed 'about us' component from main page.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { AboutUs, PageLayout } from '@components'
+import { PageLayout } from '@components'
 import type { NextPage } from 'next'
 import { useTranslation } from 'next-i18next'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
@@ -11,7 +11,6 @@ const Home: NextPage = () => {
       <div className='flex flex-col items-center justify-center h-screen bg-neutral50 dark:bg-neutral800'>
         <p className='text-4xl mb-5 text-neutral900 dark:text-neutral50'>{t('common:testNamespace')}</p>
       </div>
-      <AboutUs />
     </PageLayout>
   )
 }
