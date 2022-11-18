a85f1822e403226f61d77b2f5f60033e255a4e95
stalugaexpert <stalegapawel@gmail.com>
2022-11-18 19:35:53 +0100

    Fixed problem with GA.

--- src/config/config.ts
+++ src/config/config.ts
@@ -4,4 +4,4 @@ export const SITE_KEY = process.env.NEXT_PUBLIC_SITE_KEY
 export const SITE_SECRET = process.env.NEXT_PUBLIC_SITE_SECRET
 export const STRAPI_URL = process.env.NEXT_PUBLIC_STRAPI_URL || 'http://localhost:1337' // change to '||' if want to use production
 export const STRAPI_API_KEY = process.env.NEXT_PUBLIC_STRAPI_API_KEY
-export const TRACKING_ID = process.env.NEXT_PUBLIC_TRACKING_ID
+export const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_TRACKING_ID
