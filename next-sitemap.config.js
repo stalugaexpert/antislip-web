9d83fe4bd00828da58dd8a7e0f985ed41bc633db
stalugaexpert <stalegapawel@gmail.com>
2022-12-02 17:12:28 +0100

    Added sitemap, created automatic script that generates sitemap on build.

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,15 +2,48 @@
 module.exports = {
   siteUrl: 'https://anti-slip.io',
   generateRobotsTxt: true,
-  exclude: ['/en', '/en/*'], // (optional)
-  alternateRefs: [
-    {
-      href: 'https://anti-slip.io',
-      hreflang: 'pl',
-    },
-    {
-      href: 'https://anti-slip.io/en',
-      hreflang: 'en',
-    },
-  ],
+  transform: async (config, path) => {
+    if (path.includes('en/blog/')) {
+      return {
+        loc: path,
+        changefreq: config.changefreq,
+        priority: config.priority,
+        lastmod: config.autoLastmod ? new Date().toISOString() : undefined,
+        alternateRefs: [{ href: 'https://anti-slip.io/en',
+          hreflang: 'en', }] ?? [],
+      }
+    }
+
+    if (path.includes('blog/')) {
+      return {
+        loc: path,
+        changefreq: config.changefreq,
+        priority: config.priority,
+        lastmod: config.autoLastmod ? new Date().toISOString() : undefined,
+        alternateRefs: [{ href: 'https://anti-slip.io',
+          hreflang: 'pl', }] ?? [],
+      }
+    }
+
+    if (path.includes('/en')) {
+      return null
+    }
+
+    return {
+      loc: path,
+      changefreq: config.changefreq,
+      priority: config.priority,
+      lastmod: config.autoLastmod ? new Date().toISOString() : undefined,
+      alternateRefs: [
+        {
+          href: 'https://anti-slip.io',
+          hreflang: 'pl',
+        },
+        {
+          href: 'https://anti-slip.io/en',
+          hreflang: 'en',
+        },
+      ],
+    }
+  },
 }
