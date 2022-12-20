6152d16b64f6989673c42c2cedf2d7607aab8b20
stalugaexpert <stalegapawel@gmail.com>
2022-12-20 23:01:41 +0100

    Temporary removed page transitions.

--- src/components/Accordion/Accordion.tsx
+++ src/components/Accordion/Accordion.tsx
@@ -9,10 +9,9 @@ interface IAccordionBaseProps {
   setExpanded: Dispatch<SetStateAction<number | false>>
   children: ReactI18NextChild | Iterable<ReactI18NextChild>
   title: string
-  keyItem: number
 }
 
-const AccordionBase = ({ i, expanded, setExpanded, children, title, keyItem }: IAccordionBaseProps) => {
+const AccordionBase = ({ i, expanded, setExpanded, children, title }: IAccordionBaseProps) => {
   const isOpen = i === expanded
   const { systemTheme, theme } = useTheme()
   const [currentTheme, setCurrentTheme] = useState('light')
@@ -25,41 +24,39 @@ const AccordionBase = ({ i, expanded, setExpanded, children, title, keyItem }: I
 
   return (
     <>
-      <AnimatePresence>
-        <motion.header
-          animate={{ backgroundColor: isOpen && currentTheme === 'dark' ? "#374151" : !isOpen && currentTheme === 'dark' ? "#111827" : isOpen && currentTheme !== 'dark' ? "#cbd5e1" : "#f3f4f6" }}
-          className="w-full p-4 mb-4 cursor-pointer last:mb-0"
-          initial={false}
-          onClick={() => setExpanded(isOpen ? false : i)}
-        >
-          <div className="flex justify-between items-center">
-            <span className="text-neutral800 dark:text-neutral50 text-base about-xsm:text-sm">{title}</span>
-            <div className="ml-4">
-              <svg
-                className="w-8 h-8 about-xsm:w-6 about-xsm:h-6"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth={1.5}
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M15.042 21.672L13.684 16.6m0 0l-2.51 2.225.569-9.47 5.227 7.917-3.286-.672zm-7.518-.267A8.25 8.25 0 1120.25 10.5M8.288 14.212A5.25 5.25 0 1117.25 10.5"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                />
-              </svg>
-            </div>
+      <motion.header
+        animate={{ backgroundColor: isOpen && currentTheme === 'dark' ? "#374151" : !isOpen && currentTheme === 'dark' ? "#111827" : isOpen && currentTheme !== 'dark' ? "#cbd5e1" : "#f3f4f6" }}
+        className="w-full p-4 mb-4 cursor-pointer last:mb-0"
+        initial={false}
+        onClick={() => setExpanded(isOpen ? false : i)}
+      >
+        <div className="flex justify-between items-center">
+          <span className="text-neutral800 dark:text-neutral50 text-base about-xsm:text-sm">{title}</span>
+          <div className="ml-4">
+            <svg
+              className="w-8 h-8 about-xsm:w-6 about-xsm:h-6"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth={1.5}
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M15.042 21.672L13.684 16.6m0 0l-2.51 2.225.569-9.47 5.227 7.917-3.286-.672zm-7.518-.267A8.25 8.25 0 1120.25 10.5M8.288 14.212A5.25 5.25 0 1117.25 10.5"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+            </svg>
           </div>
-        </motion.header>
-      </AnimatePresence>
+        </div>
+      </motion.header>
       <AnimatePresence initial={false}>
         {isOpen && (
           <motion.section
             animate="open"
             exit="collapsed"
             initial="collapsed"
-            key={keyItem}
+            key="content"
             transition={{ duration: 0.5, ease: [0.04, 0.62, 0.23, 0.98] }}
             variants={{
               open: { opacity: 1, height: "fit-content" },
@@ -121,7 +118,6 @@ export const Accordion = () => {
           expanded={expanded}
           i={item.key}
           key={item.key}
-          keyItem={item.key}
           setExpanded={setExpanded}
           title={item.title}
         >
