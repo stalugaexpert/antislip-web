47de37b3330f870ae20143a73c5fd0f016996305
stalugaexpert <stalegapawel@gmail.com>
2023-08-17 11:49:15 +0200

    Fixing problems with build

--- src/components/ui/Toaster.tsx
+++ src/components/ui/Toaster.tsx
@@ -7,7 +7,7 @@ import {
   ToastProvider,
   ToastTitle,
   ToastViewport,
-} from 'src/components/ui/toast'
+} from 'src/components/ui/Toast'
 import { useToast } from 'src/hooks/use-toast'
 
 export function Toaster() {
